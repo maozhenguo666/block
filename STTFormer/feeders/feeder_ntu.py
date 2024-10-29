8170273b59d93d974c4c7944c78da8eb9f93809b
mzgcicici <155879446+maozhenguo666@users.noreply.github.com>
2024-10-29 19:18:53 +0800

    10291

---
+++ STTFormer/feeders/feeder_ntu.py
@@ -0,0 +1,91 @@
+import numpy as np
+from torch.utils.data import Dataset
+from feeders import tools
+
+
+class Feeder(Dataset):
+    def __init__(self, data_path, label_path=None, p_interval=1, split='train', random_choose=False, random_shift=False,
+                 random_move=False, random_rot=False, window_size=-1, normalization=False, debug=False, use_mmap=True,
+                 bone=False, vel=False):
+        """
+        data_path:
+        label_path:
+        split: training set or test set
+        random_choose: If true, randomly choose a portion of the input sequence
+        random_shift: If true, randomly pad zeros at the begining or end of sequence
+        random_move:
+        random_rot: rotate skeleton around xyz axis
+        window_size: The length of the output sequence
+        normalization: If true, normalize input sequence
+        debug: If true, only use the first 100 samples
+        use_mmap: If true, use mmap mode to load data, which can save the running memory
+        bone: use bone modality or not
+        vel: use motion modality or not
+        only_label: only load label for ensemble score compute
+        """
+
+        self.debug = debug
+        self.data_path = data_path
+        self.label_path = label_path
+        self.split = split
+        self.random_choose = random_choose
+        self.random_shift = random_shift
+        self.random_move = random_move
+        self.window_size = window_size
+        self.normalization = normalization
+        self.use_mmap = use_mmap
+        self.p_interval = p_interval
+        self.random_rot = random_rot
+        self.bone = bone
+        self.vel = vel
+        self.load_data()
+        if normalization:
+            self.get_mean_map()
+
+    def load_data(self):
+        # data: N C V T M
+        self.data = np.load(self.data_path)
+        self.label = np.load(self.label_path)
+        self.sample_name = ['train_' + str(i) for i in range(len(self.data))]
+
+    def get_mean_map(self):
+        data = self.data
+        N, C, T, V, M = data.shape
+        self.mean_map = data.mean(axis=2, keepdims=True).mean(axis=4, keepdims=True).mean(axis=0)
+        self.std_map = data.transpose((0, 2, 4, 1, 3)).reshape((N * T * M, C * V)).std(axis=0).reshape((C, 1, V, 1))
+
+    def __len__(self):
+        return len(self.label)
+
+    def __iter__(self):
+        return self
+
+    def __getitem__(self, index):
+        data_numpy = self.data[index]
+        label = self.label[index]
+        data_numpy = np.array(data_numpy)
+        valid_frame_num = np.sum(data_numpy.sum(0).sum(-1).sum(-1) != 0)
+        
+        if valid_frame_num == 0:
+            data_numpy = np.zeros((3, 120, 17, 2)) 
+            label = 0  
+            index = index  
+            return data_numpy, label, index
+        
+        # reshape Tx(MVC) to CTVM
+        data_numpy = tools.valid_crop_resize(data_numpy, valid_frame_num, self.p_interval, self.window_size)
+        if self.random_rot:
+            data_numpy = tools.random_rot(data_numpy)
+        if self.bone:
+            ntu_pairs = ((2, 3), (2, 5), (3, 4), (5, 6), (6, 7), (2, 8), (8, 9),(8,11),
+                    (9, 10), (5, 11), (11, 12), (12, 13), (1, 14), (1, 15), (1, 1), 
+                    (14, 16), (15, 17))
+            bone_data_numpy = np.zeros_like(data_numpy)
+            for v1, v2 in ntu_pairs:
+                bone_data_numpy[:, :, v1 - 1] = data_numpy[:, :, v1 - 1] - data_numpy[:, :, v2 - 1]
+            data_numpy = bone_data_numpy
+        if self.vel:
+            data_numpy[:, :-1] = data_numpy[:, 1:] - data_numpy[:, :-1]
+            data_numpy[:, -1] = 0
+
+        return data_numpy, label, index
